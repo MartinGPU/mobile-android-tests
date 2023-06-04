7f6c9652b2a1e1e5785508e04f1e5e3338bd4d5d
Asus <maratussr@gmail.com>
2023-06-04 22:34:56 +0400

    added feature annotation

--- src/test/java/com/marat/tests/ExistingSettingsButtonTest.java
+++ src/test/java/com/marat/tests/ExistingSettingsButtonTest.java
@@ -2,6 +2,7 @@ package com.marat.tests;
 
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
+@Feature("Settings")
 public class ExistingSettingsButtonTest extends TestBase{
 
     @DisplayName("Settings button")
